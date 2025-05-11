527313442b44cafd522ba5d039a7c50c0c01e7e4
Johnny Console <johnny.console@hotmail.com>
2025-05-11 12:44:13 -0400

    Add Board Setup Fragment Files

---
+++ app/src/main/java/com/johnnyconsole/android/jeopardygamecontroller_android/ui/setup/SetupFragment.kt
@@ -0,0 +1,42 @@
+package com.johnnyconsole.android.jeopardygamecontroller_android.ui.setup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.johnnyconsole.android.jeopardygamecontroller_android.databinding.FragmentSetupBinding
+
+class SetupFragment : Fragment() {
+
+    private var _binding: FragmentSetupBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val setupViewModel =
+            ViewModelProvider(this).get(SetupViewModel::class.java)
+
+        _binding = FragmentSetupBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        setupViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
