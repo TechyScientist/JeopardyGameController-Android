31dc8c944a71907dac4ead297a6570d8696f5ced
Johnny Console <johnny.console@hotmail.com>
2025-05-11 15:38:02 -0400

    Add basic logic to Setup Fragment, Remove unnecessary SetupViewModel

--- app/src/main/java/com/johnnyconsole/android/jeopardygamecontroller_android/ui/setup/SetupFragment.kt
+++ app/src/main/java/com/johnnyconsole/android/jeopardygamecontroller_android/ui/setup/SetupFragment.kt
@@ -3,18 +3,17 @@ package com.johnnyconsole.android.jeopardygamecontroller_android.ui.setup
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.johnnyconsole.android.jeopardygamecontroller_android.R
 import com.johnnyconsole.android.jeopardygamecontroller_android.databinding.FragmentSetupBinding
 
 class SetupFragment : Fragment() {
 
     private var _binding: FragmentSetupBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +21,34 @@ class SetupFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val setupViewModel =
-            ViewModelProvider(this).get(SetupViewModel::class.java)
-
         _binding = FragmentSetupBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        setupViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        with(binding) {
+            etRoundCategory1.hint = getString(R.string.category, 1)
+            etRoundCategory2.hint = getString(R.string.category, 2)
+            etRoundCategory3.hint = getString(R.string.category, 3)
+            etRoundCategory4.hint = getString(R.string.category, 4)
+            etRoundCategory5.hint = getString(R.string.category, 5)
+            etRoundCategory6.hint = getString(R.string.category, 6)
+            etFinalJeopardyCategory.hint = "Final Jeopardy Category"
+
+            rbSingleJeopardy.setOnClickListener {
+                llCategories.visibility = VISIBLE
+                llFinalJeopardy.visibility = INVISIBLE
+            }
+
+            rbDoubleJeopardy.setOnClickListener {
+                llCategories.visibility = VISIBLE
+                llFinalJeopardy.visibility = INVISIBLE
+            }
+
+            rbFinalJeopardy.setOnClickListener {
+                llCategories.visibility = INVISIBLE
+                llFinalJeopardy.visibility = VISIBLE
+            }
+
+            return root
         }
-        return root
     }
 
     override fun onDestroyView() {
